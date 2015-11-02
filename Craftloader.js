a559186658bd3e2e3d01b278bd1172a034c2d8dd
Saul van der Walt <saulvdw@gmail.com>
2015-11-02 18:43:39 +0200

    refactored everything , crushed bugs

--- Craftloader.js
+++ Craftloader.js
@@ -9,27 +9,25 @@
     browser: 'unknown'
   };
 
-  var ua = navigator.userAgent,
+  var PreKey = 'craft:',
+    ua = navigator.userAgent,
     tem,
     M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
   if (M && (tem = ua.match(/version\/([\.\d]+)/i)) != null) M[2] = tem[1];
   M ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
   this.CurrentBrowser.browser = M.join(' ');
 
-  var PreKey = 'craft:';
-
-  function saveModule(obj) {
+  function fetchImport(obj) {
     return new Promise((success, failure) => {
       fetch(obj.url).then(res => {
         res.text().then(content => {
           var now = +new Date();
           obj.data = content;
-          obj.noCache = obj.noCache || false;
           obj.stamp = now;
           obj.expire = now + ((obj.expire || 4500) * 60 * 60 * 1000);
-          if (obj.noCache) {
+          if (!obj.noCache) {
             try {
-              localStorage.setItem(PreKey + obj.key, JSON.stringify(obj));
+              localStorage.setItem(PreKey + (obj.key || obj.url), JSON.stringify(obj));
             } catch (e) {
               (e.name.toUpperCase().includes('QUOTA')) ? console.warn('localStorage is over QUOTA :' + e): console.warn("couldn't cache Module executing locally: " + e);
             }
@@ -40,104 +38,61 @@
     });
   }
 
-  function verifyModule(src, obj) {
-    return !src || src.expire - +new Date() < 0 || obj.unique !== src.unique;
-  }
-
   function serializeModule(obj) {
-    var source, promise;
-    if (obj.url === undefined) return false;
-    obj.key = (obj.key || obj.url);
-    source = Craftloader.get(obj.key);
+    var src, promise;
+    if (obj.url === undefined) {
+      console.error('no script/css/url found');
+      return false;
+    }
+    src = Craftloader.get(obj.url);
     obj.execute = obj.execute !== false;
-
-    if (verifyModule(source, obj)) {
+    if (!src || src.expire - +new Date() < 0 || obj.unique !== src.unique) {
       if (obj.unique) obj.url += ((obj.url.indexOf('?') > 0) ? '&' : '?') + 'unique=' + obj.unique;
-      promise = saveModule(obj);
+      promise = fetchImport(obj);
     } else {
-      source.execute = obj.execute;
-      promise = new Promise(resolve => resolve(source));
+      src.execute = obj.execute;
+      promise = new Promise(resolve => resolve(src));
     }
     return promise;
   }
 
   function execProcess(sources) {
     return sources.map(obj => {
-      if (obj.execute) execute(obj);
+      if (obj.execute) {
+        var execEl;
+        (obj.type === 'css') ? execEl = document.createElement('style'): execEl = document.createElement('script');
+        if (obj.type !== 'css') execEl.defer = obj.defer || false;
+        execEl.innerHTML = obj.data;
+        document.head.appendChild(execEl);
+      }
       return obj;
     });
   }
 
-  function execute(obj) {
-    if (obj.type === 'css') {
-      var style = document.createElement('style');
-      style.innerHTML = obj.data;
-      document.head.appendChild(style);
-    } else {
-      var script = document.createElement('script');
-      script.defer = obj.defer || true;
-      script.innerHTML = obj.data;
-      document.head.appendChild(script);
-    }
-  }
-
-  function deliverArgs() {
-    var promises = [];
-    Array.from(arguments).forEach(arg => {
-      if (arg.test) {
-        promises.push(serializeModule(arg));
-      } else if (arg.test === undefined) {
-        promises.push(serializeModule(arg));
-      }
-    });
-    return Promise.all(promises);
-  }
-
-  function requireMore() {
-    var resources = deliverArgs.apply(null, arguments);
-    var promise = this.then(() => {
-      return resources;
-    }).then(execProcess);
-    promise.requireMore = requireMore;
-    return promise;
-  }
-
   this.Craftloader = {
     require: function () {
-      var obj = [],
-        i = 0;
-      Array.from(arguments).forEach(arg => {
-        obj[i] = {};
-        obj[i].execute = arg.execute !== false;
-        if (arg.url === undefined) {
-          if (arg.script !== undefined) {
-            obj[i].url = arg.script;
-            obj[i].type = 'script';
-            arg.script = null;
-          } else if (arg.css !== undefined) {
-            obj[i].url = arg.css;
-            obj[i].type = 'css';
-            arg.css = null;
-          }
+      var obj, arg, promises = [];
+      for (arg of arguments) {
+        if ((arg.test !== undefined && arg.test === false) || (arg.execute !== undefined && arg.execute === false)) {
+          if (arg.test === false) Craftloader.remove((arg.css || arg.script || arg.url));
         } else {
-          obj[i].type = 'script';
+          obj = {
+            url: (arg.script || arg.css || arg.url),
+            type: arg.css ? 'css' : 'script'
+          };
+          if (arg.noCache === true) obj.noCache = true;
+          promises.push(serializeModule(obj));
+          if (this.CraftedModules.indexOf(obj.url) < 0) this.CraftedModules.push(obj.url);
         }
-        i++;
-        if (arg.execute !== false && this.CraftedModules.indexOf(arg.url) < 0) this.CraftedModules.push(obj.url);
-      });
-      var promise = deliverArgs.apply(null, obj).then(execProcess);
-      promise.requireMore = requireMore;
-      return promise;
+      }
+      return Promise.all(promises).then(execProcess);
     },
-    remove: key => {
-      localStorage.removeItem(PreKey + key);
-      return Craftloader;
-    },
+    remove: key => localStorage.removeItem(PreKey + key),
     get: key => {
       try {
         return JSON.parse(localStorage.getItem(PreKey + key) || false);
       } catch (e) {
-        return false;
+        return undefined;
       }
     },
     removeAll: expired => {
@@ -146,7 +101,6 @@
         key = mod.split(PreKey)[1];
         if (key && (!expired || Craftloader.get(key).expire <= now)) Craftloader.remove(key);
       }
-      return Craftloader;
     },
     CraftedModules: []
   };
