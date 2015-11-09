00a94120f98465d273f5a7ca10752703e5fcea39
Saul van der Walt <saulvdw@gmail.com>
2015-11-09 21:34:01 +0200

    fixed bugs , setPreKey for script cache

--- ES5_Version/Craftloader.js
+++ ES5_Version/Craftloader.js
@@ -2,76 +2,34 @@
 
 (function (document) {
   var pre = 'craft:',
-      def = function def(obj) {
-    return obj !== undefined;
-  },
-      doc = document;
-
-  function CraftImport(obj) {
-    var src = Craftloader.get(obj.key || obj.url),
-        promise = undefined,
-        now = +new Date();
-    obj.execute = obj.execute !== false;
-    if (!src || src.expire - now < 0 || obj.unique !== src.unique) {
-      if (obj.unique) obj.url += (obj.url.indexOf('?') > 0 ? '&' : '?') + 'unique=' + obj.unique;
-      promise = new Promise(function (success, failed) {
-        return fetch(obj.url).then(function (res) {
-          return res.text().then(function (data) {
-            obj.data = data;
-            obj.stamp = now;
-            obj.expire = now + (obj.expire || 4000) * 60 * 60 * 1000;
-            if (!obj.noCache) try {
-              localStorage.setItem(pre + (obj.key || obj.url), JSON.stringify(obj));
-            } catch (e) {
-              e.name.toUpperCase().includes('QUOTA') ? console.warn('localStorage is full: ' + e) : console.warn('Craftloader : problem caching script: ' + e);
-            }
-            success(obj);
-          });
-        }).catch(function (err) {
-          return failed('Could not fetch Craftloader import -> ' + err);
-        });
-      });
-    } else {
-      src.execute = obj.execute;
-      promise = new Promise(function (resolve) {
-        return resolve(src);
-      });
+      doc = document,
+      def = function def() {
+    for (var _len = arguments.length, obj = Array(_len), _key = 0; _key < _len; _key++) {
+      obj[_key] = arguments[_key];
     }
-    return promise;
-  }
 
-  function execute(sources) {
-    return sources.map(function (obj) {
-      if (obj.execute) {
-        var execEl = undefined;
-        obj.type === 'css' ? execEl = doc.createElement('style') : execEl = doc.createElement('script');
-        if (obj.type !== 'css') execEl.defer = obj.defer || false;
-        execEl.innerHTML = obj.data;
-        doc.head.appendChild(execEl);
-      }
-      return obj;
+    return obj.every(function (i) {
+      return i !== undefined;
     });
-  }
+  };
 
   self.Craftloader = {
     Import: function Import() {
+      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
+      }
+
       var obj = undefined,
-          args = arguments,
           promises = [],
-          i = 0;
-      for (; i < args.length; i++) if (args[i].test === false || args[i].execute === false) {
-        if (args[i].test === false) Craftloader.remove(args[i].css || args[i].script || args[i].url);
-      } else {
-        obj = {
-          url: args[i].script || args[i].css || args[i].url,
-          type: args[i].css ? 'css' : 'script'
-        };
+          i = undefined;
+      for (i in args) if (args[i].test !== false) {
+        obj = { url: args[i].css || args[i].script || args[i].url, type: args[i].css ? 'css' : 'script', exec: args[i].execute !== false };
         if (args[i].noCache === true) obj.noCache = true;
         if (def(args[i].key)) obj.key = args[i].key;
         if (def(args[i].defer)) obj.defer = args[i].defer;
         if (def(args[i].expire)) obj.expire = args[i].expire;
         promises.push(CraftImport(obj));
-      }
+      } else Craftloader.remove(args[i].css || args[i].script || args[i].url);
       return Promise.all(promises).then(execute);
     },
     setPrekey: function setPrekey(str) {
@@ -93,5 +51,44 @@
       }
     }
   };
+
+  function CraftImport(obj) {
+    var promise = undefined,
+        now = +new Date(),
+        key = obj.key || obj.url,
+        src = Craftloader.get(key);
+    if (!src || src.expire - now < 0 || obj.unique !== src.unique) {
+      if (obj.unique) obj.url += (obj.url.indexOf('?') > 0 ? '&' : '?') + 'unique=' + obj.unique;
+      promise = new Promise(function (success, failed) {
+        return fetch(obj.url).then(function (res) {
+          return res.text().then(function (data) {
+            obj.data = data;
+            obj.stamp = now;
+            obj.expire = now + (obj.expire || 4000) * 60 * 60 * 1000;
+            if (!obj.noCache) localStorage.setItem(pre + key, JSON.stringify(obj));
+            success(obj);
+          });
+        }).catch(function (err) {
+          return failed('Craftloader: problem fetching import -> ' + err);
+        });
+      });
+    } else promise = new Promise(function (resolve) {
+      return resolve(src);
+    });
+    return promise;
+  }
+
+  function execute(sources) {
+    return sources.map(function (obj) {
+      if (obj.exec) {
+        var execEl = undefined;
+        obj.type === 'css' ? execEl = doc.createElement('style') : execEl = doc.createElement('script');
+        if (obj.type !== 'css') execEl.defer = obj.defer || false;
+        execEl.innerHTML = obj.data;
+        doc.head.appendChild(execEl);
+        return obj;
+      }
+    });
+  }
   Craftloader.removeAll(true);
 })(document);
