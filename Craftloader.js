a2f100919087b4e340400fbbc251b6e90052d9b8
Saul van der Walt <saulvdw@gmail.com>
2015-11-10 09:24:32 +0200

    refactored, only 52 lines long

--- Craftloader.js
+++ Craftloader.js
@@ -6,28 +6,25 @@
     Import: (...args) => {
       let obj, promises = [], i;
       for (i in args) if (args[i].test !== false) {
-          obj = { url: (args[i].css || args[i].script || args[i].url), type: args[i].css ? 'css' : 'script' , exec : args[i].execute !== false};
+          obj = { url: (args[i].css || args[i].script), type: args[i].css ? 'css' : 'script' , exec : args[i].execute !== false};
           if (args[i].noCache === true) obj.noCache = true;
           if (def(args[i].key)) obj.key = args[i].key;
           if (def(args[i].defer)) obj.defer = args[i].defer;
           if (def(args[i].expire)) obj.expire = args[i].expire;
           promises.push(CraftImport(obj));
-        } else Craftloader.remove((args[i].css || args[i].script || args[i].url));
+        } else Craftloader.remove((args[i].css || args[i].script));
       return Promise.all(promises).then(execute);
     },
     setPrekey: str => pre = str + ':',
-    get: key => JSON.parse(localStorage.getItem(pre + key) || false),
-    remove: key => localStorage.removeItem(pre + key),
+    get: key => JSON.parse(localStorage.getItem(key.includes(pre) ? key : pre + key) || false),
+    remove: key => localStorage.removeItem(key.includes(pre) ? key : pre + key),
     removeAll: expired => {
-      let key, i, now = +new Date();
-      for (i in localStorage) {
-        key = i.split(pre)[1];
-        if (key && (!expired || Craftloader.get(key).expire <= now)) Craftloader.remove(key);
-      }
+      let i, now = +new Date();
+      for (i in localStorage) if (!expired || Craftloader.get(i).expire <= now) Craftloader.remove(i);
     }
   };
 
-  function CraftImport(obj) {
+  var CraftImport = obj => {
     let promise, now = +new Date(), key = (obj.key || obj.url), src = Craftloader.get(key);
     if (!src || src.expire - now < 0 || obj.unique !== src.unique) {
       if (obj.unique) obj.url += ((obj.url.indexOf('?') > 0) ? '&' : '?') + 'unique=' + obj.unique;
@@ -42,17 +39,13 @@
     return promise;
   }
 
-  function execute(sources) {
-    return sources.map(obj => {
-      if(obj.exec) {
-        let execEl;
-        (obj.type === 'css') ? execEl = doc.createElement('style'): execEl = doc.createElement('script');
-        if (obj.type !== 'css') execEl.defer = obj.defer || false;
-        execEl.innerHTML = obj.data;
-        doc.head.appendChild(execEl);
+  var execute = src => src.map(obj => {
+        let el;
+        (obj.type === 'css') ? el = doc.createElement('style'): el = doc.createElement('script');
+        if (obj.type !== 'css') el.defer = obj.defer || false;
+        el.innerHTML = obj.data;
+        if(obj.exec) doc.head.appendChild(el);
         return obj;
-      }
-    });
-  }
+  });
   Craftloader.removeAll(true);
 })(document);
