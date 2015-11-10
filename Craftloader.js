9a8c0a78d3116620fd9b89ca36963df02ecfe127
Saul van der Walt <saulvdw@gmail.com>
2015-11-10 18:49:51 +0200

    condensed to the max 39 lines

--- Craftloader.js
+++ Craftloader.js
@@ -1,51 +1,39 @@
 'use strict';
-(document => {
-  var pre = 'craft:', doc = document, def = (...obj) => obj.every(i => i !== undefined);
-
+(() => {
+  var pre = 'craft:', doc = document, def = (...obj) => obj.every(o => o !== undefined),
+  CraftImport = obj => {
+    let now = +new Date(), key = (obj.key || obj.url),src = Craftloader.get(key);
+    if (src || src.expire - now > 0) return new Promise(resolve => resolve(src));
+    return new Promise((success, failed) => fetch(obj.url).then(res => res.text().then(data => {
+      obj.data = data;
+      obj.stamp = now;
+      obj.expire = now + ((obj.expire || 4000) * 60 * 60 * 1000);
+      if (obj.cache) localStorage.setItem(pre + key, JSON.stringify(obj));
+      success(obj);
+    })).catch(err => failed(`Craftloader: problem fetching import -> ${err}`)));
+  },
+  execute = src => src.map(obj => {
+    let el = (obj.type === 'css') ? doc.createElement('style') : doc.createElement('script');
+    el.defer = obj.defer || undefined;
+    el.innerHTML = obj.data;
+    if (obj.exec) doc.head.appendChild(el);
+  });
   self.Craftloader = {
     Import: (...args) => {
-      let obj, promises = [], i;
-      for (i in args) if (args[i].test !== false) {
-          obj = { url: (args[i].css || args[i].script), type: args[i].css ? 'css' : 'script' , exec : args[i].execute !== false};
-          if (args[i].noCache === true) obj.noCache = true;
-          if (def(args[i].key)) obj.key = args[i].key;
-          if (def(args[i].defer)) obj.defer = args[i].defer;
-          if (def(args[i].expire)) obj.expire = args[i].expire;
-          promises.push(CraftImport(obj));
-        } else Craftloader.remove((args[i].css || args[i].script));
+      let obj, promises = [];
+      args.forEach(arg => {
+        obj = {url: (arg.css || arg.script), type: arg.css ? 'css' : 'script', exec: arg.execute !== false, cache: arg.noCache !== false};
+        if (def(arg.key)) obj.key = arg.key;
+        if (def(arg.defer)) obj.defer = arg.defer;
+        if (def(arg.expire)) obj.expire = arg.expire;
+        arg.test === false ? Craftloader.remove(obj.url) : promises.push(CraftImport(obj));
+      });
       return Promise.all(promises).then(execute);
     },
     setPrekey: str => pre = str + ':',
     get: key => JSON.parse(localStorage.getItem(key.includes(pre) ? key : pre + key) || false),
     remove: key => localStorage.removeItem(key.includes(pre) ? key : pre + key),
-    removeAll: expired => {
-      let i, now = +new Date();
-      for (i in localStorage) if (!expired || Craftloader.get(i).expire <= now) Craftloader.remove(i);
-    }
+    removeAll: expired => { for (let i in localStorage) if (!expired || Craftloader.get(i).expire <= +new Date()) Craftloader.remove(i) }
   };
-
-  var CraftImport = obj => {
-    let promise, now = +new Date(), key = (obj.key || obj.url), src = Craftloader.get(key);
-    if (!src || src.expire - now < 0 || obj.unique !== src.unique) {
-      if (obj.unique) obj.url += ((obj.url.indexOf('?') > 0) ? '&' : '?') + 'unique=' + obj.unique;
-        promise = new Promise((success, failed) => fetch(obj.url).then(res => res.text().then(data => {
-        obj.data = data;
-        obj.stamp = now;
-        obj.expire = now + ((obj.expire || 4000) * 60 * 60 * 1000);
-        if(!obj.noCache) localStorage.setItem(pre + key, JSON.stringify(obj));
-        success(obj);
-      })).catch(err => failed(`Craftloader: problem fetching import -> ${err}`)));
-    } else promise = new Promise(resolve => resolve(src));
-    return promise;
-  }
-
-  var execute = src => src.map(obj => {
-        let el;
-        (obj.type === 'css') ? el = doc.createElement('style'): el = doc.createElement('script');
-        if (obj.type !== 'css') el.defer = obj.defer || false;
-        el.innerHTML = obj.data;
-        if(obj.exec) doc.head.appendChild(el);
-        return obj;
-  });
   Craftloader.removeAll(true);
-})(document);
+})();
