95be80cf0047f296097a84ae29805052b358e8e8
Saul van der Walt <saulvdw@gmail.com>
2016-01-08 13:37:11 +0200

    new and improved , better compatibility

--- Craftloader.js
+++ Craftloader.js
@@ -1,39 +1,44 @@
 'use strict';
-(() => {
-  var pre = 'craft:', doc = document, def = (...obj) => obj.every(o => o !== undefined),
-  CraftImport = obj => {
-    let now = +new Date(), key = (obj.key || obj.url),src = Craftloader.get(key);
-    if (src || src.expire - now > 0) return new Promise(resolve => resolve(src));
-    return new Promise((success, failed) => fetch(obj.url).then(res => res.text().then(data => {
-      obj.data = data;
-      obj.stamp = now;
-      obj.expire = now + ((obj.expire || 4000) * 60 * 60 * 1000);
-      if (obj.cache) localStorage.setItem(pre + key, JSON.stringify(obj));
-      success(obj);
-    })).catch(err => failed(`Craftloader: problem fetching import -> ${err}`)));
-  },
-  execute = src => src.map(obj => {
-    let el = doc.createElement(obj.type === 'css' ? 'style' : 'script');
-    el.defer = obj.defer || undefined;
-    el.innerHTML = obj.data;
-    if (obj.exec) doc.head.appendChild(el);
-  });
+((doc) => {
   self.Craftloader = {
-    Import: (...args) => {
-      let obj, promises = [];
-      args.forEach(arg => {
-        obj = {url: (arg.css || arg.script), type: arg.css ? 'css' : 'script', exec: arg.execute !== false, cache: arg.noCache !== false};
-        if (def(arg.key)) obj.key = arg.key;
-        if (def(arg.defer)) obj.defer = arg.defer;
-        if (def(arg.expire)) obj.expire = arg.expire;
-        arg.test === false ? Craftloader.remove(obj.url) : promises.push(CraftImport(obj));
-      });
-      return Promise.all(promises).then(execute);
+    pre: 'craft:',
+    fetchImport(obj) {
+      obj.key = obj.key || obj.url;
+      let now = +new Date(),
+        src = Craftloader.get(obj.key);
+      if (src || src.expire - now > 0) return new Promise(resolve => resolve(src));
+      return new Promise((pass, fail) => fetch(obj.url).then(res => res.text().then(data => {
+        obj.data = data;
+        obj.stamp = now;
+        obj.expire = now + ((obj.expire || 4000) * 60 * 60 * 1000);
+        if (obj.cache) localStorage.setItem(Craftloader.pre + obj.key, JSON.stringify(obj));
+        pass(obj);
+      })).catch(err => fail(`error importing -> ${err}`)));
     },
-    setPrekey: str => pre = str + ':',
-    get: key => JSON.parse(localStorage.getItem(key.includes(pre) ? key : pre + key) || false),
-    remove: key => localStorage.removeItem(key.includes(pre) ? key : pre + key),
-    removeAll: expired => { for (let i in localStorage) if (!expired || Craftloader.get(i).expire <= +new Date()) Craftloader.remove(i) }
-  };
+    set prekey(str) {Craftloader.pre = str + ':'},
+    get: key => JSON.parse(localStorage.getItem(key.includes(Craftloader.pre) ? key : Craftloader.pre + key) || false),
+    remove: key => localStorage.removeItem(key.includes(Craftloader.pre) ? key : Craftloader.pre + key),
+    removeAll(expired) { for (let i in localStorage) if (!expired || Craftloader.get(i).expire <= +new Date()) Craftloader.remove(i) },
+    Import() {
+      let promises = [];
+      Array.prototype.slice.call(arguments).forEach(arg => arg.test === false ? Craftloader.remove(arg.css || arg.script) : promises.push(Craftloader.fetchImport({
+        url: arg.css || arg.script,
+        type: arg.css ? 'css' : 'script',
+        exec: arg.execute !== false,
+        cache: arg.cache !== false,
+        defer: arg.defer ? 'defer' : null,
+        key: arg.key,
+        expire: arg.expire
+      })));
+      return Promise.all(promises).then(src => src.map(obj => {
+        if (!obj.exec) return;
+        let el = doc.createElement(obj.type === 'css' ? 'style' : 'script');
+        el.defer = obj.defer;
+        obj.type === 'css' ? el.textContent = obj.data : el.src = URL.createObjectURL(new Blob([obj.data]));
+        if (obj.key) el.setAttribute('key', obj.key);
+        doc.head.appendChild(el);
+      }));
+    }
+  }
   Craftloader.removeAll(true);
-})();
+})(document);
